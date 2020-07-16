ce791a6cf1886705f8c98148b2a808432aabc162
priya-dream <n.priya199720@gmail.com>
2020-07-16 20:53:56 +0530

    test

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -48,7 +48,7 @@
             <div class="collapse" id="ui-basic">
               <ul class="nav flex-column sub-menu">
                 <li class="nav-item">
-                  <a class="nav-link" href="pages/ui-features/buttons.html">Button</a>
+                  <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a>
